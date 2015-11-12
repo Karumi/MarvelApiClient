11a083af96a1c26d19d4f56ea22282b97ed866c5
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-11-12 16:46:04 +0100

    Initial Commit

---
+++ MarvelApiClient/MarvelApiClient.h
@@ -0,0 +1,19 @@
+//
+//  MarvelApiClient.h
+//  MarvelApiClient
+//
+//  Created by Pedro Vicente Gomez on 12/11/15.
+//  Copyright © 2015 GoKarumi S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarvelApiClient.
+FOUNDATION_EXPORT double MarvelApiClientVersionNumber;
+
+//! Project version string for MarvelApiClient.
+FOUNDATION_EXPORT const unsigned char MarvelApiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelApiClient/PublicHeader.h>
+
+
