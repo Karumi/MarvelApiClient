a8e1eb4bd1ff74c5cdb3b0064d80bedbabfeb562
pedrovgs <pedrovicente.gomez@gmail.com>
2015-11-14 20:39:12 +0100

    Change CharactersApiClient to use BaseApiClient instead of a manual request based on a http client

--- MarvelApiClient/CharactersApiClient.swift
+++ MarvelApiClient/CharactersApiClient.swift
@@ -12,13 +12,11 @@ import BrightFutures
 public class CharactersApiClient : BaseApiClient {
     
     public func getAll() -> Future<[Character],NSError> {
-        let request = HttpRequest(url: "http://gateway.marvel.com:80/v1/public/characters?apikey=54355f684e1983a183d7bfec96a4bf81", parameters: [String:String](), headers: [String:String](), verb: .GET)
-        return httpClient.send(request).map { response -> [Character] in
+        return sendRequest(.GET, path: "characters").map { response -> [Character] in
             print(response.statusCode)
             print(response.body)
             return [Character]()
         }
-        
     }
     
 }
